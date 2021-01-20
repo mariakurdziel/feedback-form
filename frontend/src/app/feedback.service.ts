7300f2d986529751900f9522af4e1ccb5de6195d
Maria <marysiazk@poczta.onet.pl>
2021-01-20 18:19:19 +0100

    Removing one unused import to make code more neat

--- frontend/src/app/feedback.service.ts
+++ frontend/src/app/feedback.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import {Feedback} from "./feedback";
 
 @Injectable({
   providedIn: 'root'
